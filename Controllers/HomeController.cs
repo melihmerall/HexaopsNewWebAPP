08e682313e1fdd0f9c02522022d0508f7b2f2a44
melihmerall <melih16.meral@gmail.com>
2024-02-09 00:14:27 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using HexaopsNewWebAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HexaopsNewWebAPP.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+		[Route("/")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("/hakkinda")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
