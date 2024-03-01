335544b5ecf684ab4a76d82a88712e054d883cce
oguzkaancinar <kaanoguzcinar@gmail.com>
2024-03-01 15:28:06 +0300

    okey

--- Entities/MainSolutions.cs
+++ Entities/MainSolutions.cs
@@ -4,6 +4,12 @@ namespace HexaopsNewWebAPP.Entities
 {
 	public class MainSolutions: BaseEntity
 	{
+		
+
+		public List<MainSolutionsAsssoc> MainSolutionsAssoc {  get; set; }
+
+
+
 		public string? SolutionsImagePath { get; set; }
 		public string? SolutionsTitle { get; set; }
 		public string? SolutionsDescription { get; set; }
